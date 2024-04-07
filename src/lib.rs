6a3113095858092cc29a28e73f6984dd847b7b91
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-04-07 13:25:45 +0200

    Move test keys into tests

--- src/lib.rs
+++ src/lib.rs
@@ -774,35 +774,47 @@ mod non_std_tests {
         }
 
         let test_cases: &[(&[u8], fn(&PrivateKeyDer<'_>) -> bool); 11] = &[
-            (&include_bytes!("test_keys/eddsakey.der")[..], is_pkcs8),
-            (&include_bytes!("test_keys/nistp256key.der")[..], is_sec1),
+            (&include_bytes!("../tests/keys/eddsakey.der")[..], is_pkcs8),
             (
-                &include_bytes!("test_keys/nistp256key.pkcs8.der")[..],
-                is_pkcs8,
+                &include_bytes!("../tests/keys/nistp256key.der")[..],
+                is_sec1,
             ),
-            (&include_bytes!("test_keys/nistp384key.der")[..], is_sec1),
             (
-                &include_bytes!("test_keys/nistp384key.pkcs8.der")[..],
-                is_pkcs8,
-            ),
-            (&include_bytes!("test_keys/nistp521key.der")[..], is_sec1),
-            (
-                &include_bytes!("test_keys/nistp521key.pkcs8.der")[..],
+                &include_bytes!("../tests/keys/nistp256key.pkcs8.der")[..],
                 is_pkcs8,
             ),
             (
-                &include_bytes!("test_keys/rsa2048key.pkcs1.der")[..],
+                &include_bytes!("../tests/keys/nistp384key.der")[..],
+                is_sec1,
+            ),
+            (
+                &include_bytes!("../tests/keys/nistp384key.pkcs8.der")[..],
+                is_pkcs8,
+            ),
+            (
+                &include_bytes!("../tests/keys/nistp521key.der")[..],
+                is_sec1,
+            ),
+            (
+                &include_bytes!("../tests/keys/nistp521key.pkcs8.der")[..],
+                is_pkcs8,
+            ),
+            (
+                &include_bytes!("../tests/keys/rsa2048key.pkcs1.der")[..],
                 is_pkcs1,
             ),
             (
-                &include_bytes!("test_keys/rsa2048key.pkcs8.der")[..],
+                &include_bytes!("../tests/keys/rsa2048key.pkcs8.der")[..],
                 is_pkcs8,
             ),
             (
-                &include_bytes!("test_keys/rsa4096key.pkcs8.der")[..],
+                &include_bytes!("../tests/keys/rsa4096key.pkcs8.der")[..],
                 is_pkcs8,
             ),
-            (&include_bytes!("test_keys/edd25519_v2.der")[..], is_pkcs8),
+            (
+                &include_bytes!("../tests/keys/edd25519_v2.der")[..],
+                is_pkcs8,
+            ),
         ];
 
         for (key_bytes, expected_check_fn) in test_cases.iter() {
