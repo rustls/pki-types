4772ea58016630453900af479a74d24add782348
Joe Birr-Pixton <jpixton@gmail.com>
2026-03-04 09:16:04 +0000

    Update ECH reference to RFC 9849

--- src/lib.rs
+++ src/lib.rs
@@ -787,7 +787,7 @@ impl SubjectPublicKeyInfoDer<'_> {
 }
 
 /// A TLS-encoded Encrypted Client Hello (ECH) configuration list (`ECHConfigList`); as specified in
-/// [draft-ietf-tls-esni-18 §4](https://datatracker.ietf.org/doc/html/draft-ietf-tls-esni-18#section-4)
+/// [RFC 9849 §4](https://datatracker.ietf.org/doc/html/rfc9849#section-4)
 #[derive(Clone, Eq, Hash, PartialEq)]
 pub struct EchConfigListBytes<'a>(BytesInner<'a>);
 
